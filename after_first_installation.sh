6cc5b47d801542b3546b42f7b6f015e60d099f67
Rafail Drakakis <csd5310@csd.uoc.gr>
2023-04-01 14:08:55 +0300

    correction in pip installation

--- after_first_installation.sh
+++ after_first_installation.sh
@@ -10,14 +10,15 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
 
 # Install software
-sudo apt install software-properties-common apt-transport-https filezilla copyq wget clang-format-11 python3 python3-pip qbittorrent g++ cmake vlc git tree htop nmap ssh screen unzip curl gparted vim -y
+sudo apt install software-properties-common apt-transport-https filezilla copyq wget clang-format-11 python3 python3-pip qbittorrent g++ cmake vlc git tree htop nmap ssh screen unzip curl gparted vim ffmpeg yt-dlp jupyter -y
 sudo snap install notepad-plus-plus code -y
-pip install numpy pandas matplotlib requests beautifulsoup4 pygame pytube openpyxl yt-dlp jupyter
+pip install numpy pandas matplotlib requests beautifulsoup4 pygame pytube openpyxl 
 
 # Set up aliases for frequently used commands
 echo "alias jupyter='jupyter notebook'" >> ~/.bashrc # Enter jupyter instead of jupyter notebook
 echo "alias google='google-chrome'" >> ~/.bashrc # Enter google instead of google-chrome
 echo "alias python='python3'" >> ~/.bashrc
+echo "alias pip='pip3'" >> ~/.bashrc
 echo "MAKEFLAGS= \"-J$(nproc)\"" # This command sets the number of jobs make can run simultaneously
 
 # Final system update
