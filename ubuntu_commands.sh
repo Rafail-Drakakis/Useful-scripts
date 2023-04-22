a1eda6daa3827b4b3ae1628b968d10fa322e1bae
Rafail Drakakis <csd5310@csd.uoc.gr>
2023-04-22 15:32:21 +0300

    add python libraries

--- ubuntu_commands.sh
+++ ubuntu_commands.sh
@@ -12,13 +12,13 @@ rm google-chrome-stable_current_amd64.deb
 # Install software
 sudo apt install software-properties-common apt-transport-https filezilla copyq wget clang-format-11 python3 python3-pip qbittorrent g++ cmake vlc git tree htop nmap ssh screen unzip curl gparted vim ffmpeg yt-dlp jupyter tesseract-ocr -y
 sudo snap install notepad-plus-plus code -y
-pip install numpy pandas matplotlib requests beautifulsoup4 pygame pytube openpyxl pytesseract Pillow pydub sklearn
+pip install numpy pandas matplotlib requests beautifulsoup4 pygame pytube openpyxl pytesseract Pillow pydub sklearn os shutil itertools time random sys re docx urllib moviepy pdf2docx PyPDF2 pdf2image pathlib
 
 # Set up aliases for frequently used commands
 echo "alias jupyter='jupyter notebook'" >> ~/.bashrc # Enter jupyter instead of jupyter notebook
 echo "alias google='google-chrome'" >> ~/.bashrc # Enter google instead of google-chrome
-echo "alias python='python3'" >> ~/.bashrc
-echo "alias pip='pip3'" >> ~/.bashrc
+echo "alias python='python3'" >> ~/.bashrc # Enter python instead of python3
+echo "alias pip='pip3'" >> ~/.bashrc # Enter pip instead of pip3
 echo "MAKEFLAGS= \"-J$(nproc)\"" # This command sets the number of jobs make can run simultaneously
 
 # Final system update
