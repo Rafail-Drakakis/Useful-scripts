6146404126510bacdc3c34f8ae11176bde5e1a7e
Rafail Drakakis <csd5310@csd.uoc.gr>
2023-05-15 12:43:49 +0300

    add snap and sublime text

--- ubuntu_commands.sh
+++ ubuntu_commands.sh
@@ -5,13 +5,15 @@ gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM # Mov
 sudo apt install gnome-shell-extension-prefs gnome-tweaks ttf-mscorefonts-installer -y # Install gnome-tweaks and microsoft fonts
 
 # Install Google Chrome 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb 
-rm google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb #download the .deb file
+sudo dpkg -i google-chrome-stable_current_amd64.deb #install google chrome
+rm google-chrome-stable_current_amd64.deb #remove the file
 
 # Install software
-sudo apt install software-properties-common apt-transport-https filezilla copyq wget clang-format-11 python3 python3-pip qbittorrent g++ cmake vlc git tree htop nmap ssh screen unzip curl gparted vim ffmpeg yt-dlp jupyter tesseract-ocr -y
-sudo snap install notepad-plus-plus code -y
+sudo apt install software-properties-common apt-transport-https filezilla copyq wget clang-format-11 python3 python3-pip qbittorrent g++ cmake vlc git tree htop nmap ssh screen unzip curl gparted vim ffmpeg yt-dlp jupyter tesseract-ocr snapd -y
+sudo snap install notepad-plus-plus code sublime-text -y
+
+#Install python libraries
 pip install numpy pandas matplotlib requests beautifulsoup4 pygame pytube openpyxl pytesseract Pillow pydub sklearn shutil itertools time random sys re docx urllib moviepy pdf2docx PyPDF2 pdf2image pathlib
 
 # Set up aliases for frequently used commands
