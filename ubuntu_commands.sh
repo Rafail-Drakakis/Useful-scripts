f8065c5d44db779de296f0c3824b488a230b894a
Rafail Drakakis <rafaildrakakis123@gmail.com>
2023-05-23 09:19:07 +0300

    add commands to install virtualbox

--- ubuntu_commands.sh
+++ ubuntu_commands.sh
@@ -1,13 +1,18 @@
-#Ubuntu logistics
+# Ubuntu logistics
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM # Move taskbar to bottom
-gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true #Move the "show applications key to the right"
+gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true # Move the "show applications" button to the right
 sudo apt install gnome-shell-extension-prefs gnome-tweaks ttf-mscorefonts-installer -y # Install gnome-tweaks and microsoft fonts
 
-#Install sublime-text
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install sublime-text
+# Install sublime-text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null # Import Sublime Text GPG key
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list # Add Sublime Text repository to apt sources
+sudo apt-get update # Update package lists
+sudo apt-get install sublime-text # Install Sublime Text
+
+# Install virtualbox
+wget https://download.virtualbox.org/virtualbox/7.0.8/virtualbox-7.0_7.0.8-156879~Ubuntu~jammy_amd64.deb # Download the VirtualBox .deb file
+sudo dpkg -i virtualbox-7.0_7.0.8-156879~Ubuntu~jammy_amd64.deb # Install the virtualbox
+rm virtualbox-7.0_7.0.8-156879~Ubuntu~jammy_amd64.deb # Remove the .deb file
 
 # Install Google Chrome 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb #download the .deb file
@@ -18,7 +23,7 @@ rm google-chrome-stable_current_amd64.deb #remove the file
 sudo apt install software-properties-common apt-transport-https filezilla copyq wget clang-format-11 python3 python3-pip qbittorrent g++ cmake vlc git tree htop nmap ssh screen unzip curl gparted vim ffmpeg yt-dlp jupyter tesseract-ocr snapd -y
 sudo snap install notepad-plus-plus code -y
 
-#Install python libraries
+# Install python libraries
 pip install numpy pandas matplotlib requests beautifulsoup4 pygame pytube openpyxl pytesseract Pillow pydub shutil itertools time random sys re docx urllib moviepy pdf2docx PyPDF2 pdf2image pathlib
 
 # Set up aliases for frequently used commands
@@ -32,4 +37,4 @@ echo "MAKEFLAGS= \"-J$(nproc)\"" # This command sets the number of jobs make can
 sudo apt-get update -y # Update the system
 sudo apt-get upgrade -y # Upgrade the system
 sudo apt-get dist-upgrade -y # Upgrade the distribution
-sudo reboot # Reboot the system
+#sudo reboot # Reboot the system
