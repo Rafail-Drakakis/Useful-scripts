853420fd40d8bcd11839d9d83bd3e009ba70d8d8
Rafail Drakakis <rafaildrakakis123@gmail.com>
2023-05-27 18:47:59 +0300

    added more alias to the simple script

--- Simple_script.sh
+++ Simple_script.sh
@@ -10,10 +10,13 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb # Install google chrome
 rm google-chrome-stable_current_amd64.deb # Remove the file
 
 # Install software
-sudo apt install software-properties-common apt-transport-https filezilla copyq wget clang-format-11 python3 python3-pip qbittorrent g++ cmake vlc git tree htop nmap ssh screen unzip curl gparted vim ffmpeg yt-dlp jupyter tesseract-ocr snapd gnome-sound-recorder -y
+sudo apt install software-properties-common apt-transport-https copyq wget python3 python3-pip qbittorrent g++ cmake vlc git tree htop nmap ssh screen unzip curl gparted vim ffmpeg yt-dlp jupyter tesseract-ocr snapd gnome-sound-recorder -y
 
 # Set up aliases for frequently used commands
 echo "MAKEFLAGS= \"-J$(nproc)\"" # This command sets the number of jobs make can run simultaneously
+echo "alias update='sudo apt-get update -y'" >> ~/.bashrc # Type "update" instead of "sudo apt-get update -y"
+echo "alias upgrade='sudo apt-get upgrade -y'" >> ~/.bashrc # Type "upgrade" instead of "sudo apt-get upgrade -y"
+echo "alias youtube downloader='python youtube_downloader.py'" # Type "youtube downloader" instead of "python youtube_downloader.py"
 
 # Final system update
 sudo apt-get update -y # Update the system
