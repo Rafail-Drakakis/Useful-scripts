a2123bbf37f0199ecb071789be7fb545fd1010f6
Rafail-Drakakis <rafaildrakakis123@gmail.com>
2023-09-29 09:20:25 +0300

    update scripts

--- advanced_script.sh
+++ advanced_script.sh
@@ -2,24 +2,22 @@
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM' # Move taskbar to bottom
 gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true  # Move the "show applications" button to the left
 
-# sublime-text
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null # Import Sublime Text GPG key
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list # Add Sublime Text repository to apt sources
+# Install java
+sudo apt install default-jdk
 
 # Install software
 sudo add-apt-repository universe
 sudo apt install gnome-shell-extension-prefs gnome-tweaks ttf-mscorefonts-installer sublime-text shutter software-properties-common apt-transport-https filezilla copyq wget clang-format-11 python3 python3-pip qbittorrent g++ cmake vlc git tree htop nmap ssh screen unzip curl gparted vim ffmpeg jupyter tesseract-ocr snapd gnome-sound-recorder -y
-sudo snap install notepad-plus-plus code -y
-
-# Install python libraries
-pip install pyinstaller yt-dlp numpy pandas matplotlib requests beautifulsoup4 pygame pytube openpyxl pytesseract Pillow time random sys re docx urllib moviepy pdf2docx PyPDF2 pdf2image pathlib
+sudo snap install code --classic
+sudo snap install sublime-text --classic
 
 # Install Google Chrome 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb #download the .deb file
 sudo dpkg -i google-chrome-stable_current_amd64.deb #install google chrome
 rm google-chrome-stable_current_amd64.deb #remove the file
 
-wget https://sourceforge.net/projects/orthos-spell/files/v.0.4.0./orthos-el_GR-0.4.0-87.oxt # Download orthos.oxt, a Greek dictionary for Libre Office
+# Download orthos.oxt, a Greek dictionary for Libre Office
+wget https://sourceforge.net/projects/orthos-spell/files/v.0.4.0./orthos-el_GR-0.4.0-87.oxt 
 
 # Set up aliases for frequently used commands
 echo "alias python='python3'" >> ~/.bashrc # Enter python instead of python3
