03ba23c53be184a01a2e7382a79f92981a24e1b9
Rafail-Drakakis <rafaildrakakis123@gmail.com>
2024-05-10 08:27:29 +0300

    renamed files

--- configurator.sh
+++ configurator.sh
@@ -5,21 +5,14 @@ gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true  # M
 # Add the "universe" repository
 sudo add-apt-repository universe -y
 
-# Install libreoffice
-sudo apt install libreoffice -y
-
 # Download orthos, a Greek dictionary for libreoffice
 wget -O orthos-el_GR-0.4.0-87.oxt "https://downloads.sourceforge.net/project/orthos-spell/v.0.4.0./orthos-el_GR-0.4.0-87.oxt?ts=gAAAAABmL1_5wk2L8cl0R0BYsayKcpO8pLhEsem7pcm9uuuJ40-cYuiMDHbmVIqkIzB97OGtTjkXs-aIxzXpOCfeNyi-4r8L7g%3D%3D&r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Forthos-spell%2Ffiles%2Flatest%2Fdownload"
 
 # Install software
-sudo apt update && sudo apt install -y gnome-shell-extension-prefs gnome-tweaks ttf-mscorefonts-installer shutter software-properties-common apt-transport-https filezilla copyq wget clang-format-11 python3 python3-pip qbittorrent g++ cmake vlc git tree htop nmap ssh screen unzip curl gparted vim ffmpeg jupyter-notebook tesseract-ocr snapd gnome-sound-recorder
-
-# Install JDK
-sudo apt install -y default-jdk
+sudo apt install -y gnome-shell-extension-prefs gnome-tweaks ttf-mscorefonts-installer shutter software-properties-common apt-transport-https libreoffice default-jdk filezilla copyq wget clang-format-11 python3 python3-pip qbittorrent g++ cmake vlc git tree htop nmap ssh screen unzip curl gparted vim ffmpeg jupyter-notebook tesseract-ocr snapd gnome-sound-recorder
 
 # Install VS Code and IntelliJ IDEA via snap
-sudo snap install code --classic
-sudo snap install intellij-idea-community --classic --edge
+sudo snap install code --classic && sudo snap install intellij-idea-community --classic --edge
 
 # Install Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -27,16 +20,17 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb || sudo apt install -f
 rm google-chrome-stable_current_amd64.deb
 
 # Python libraries installation via pip
-source ~/my_venv/bin/activate
-pip3 install ffprobe PyPDF2 SpeechRecognition urllib3 matplotlib beautifulsoup4 ffmpeg tk pygame phonenumbers python-docx openpyxl numpy customtkinter ctkmessagebox qrcode pandas requests Pillow pdf2image moviepy pyshorteners pdf2docx yt-dlp tabula-py pytesseract opencv-python folium rasterio
+#!/bin/sh
+. source ~/my_venv/bin/activate
+pip3 install torch torchvision torchaudio ffprobe PyPDF2 SpeechRecognition urllib3 matplotlib beautifulsoup4 ffmpeg tk pygame phonenumbers python-docx openpyxl numpy customtkinter ctkmessagebox qrcode pandas requests Pillow pdf2image moviepy pyshorteners pdf2docx yt-dlp tabula-py pytesseract opencv-python folium rasterio punctuators
 deactivate
 
 # Set up aliases and environment variables
+echo "export MAKEFLAGS=\"-j\$(nproc)\"" >> ~/.bashrc
 echo "alias python='python3'" >> ~/.bashrc
 echo "alias pip='pip3'" >> ~/.bashrc
-echo "export MAKEFLAGS=\"-j\$(nproc)\"" >> ~/.bashrc
 echo "alias update='sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y'" >> ~/.bashrc
-echo "alias activate='source ~/my_venv/bin/activate'" >> ~/.bashrc
+echo "alias activate='source ~/my_venv/bin/activate'" >>    
 echo "alias connect='nordvpn connect'" >> ~/.bashrc
 echo "alias disconnecte='nordvpn disconnect'" >> ~/.bashrc
 
