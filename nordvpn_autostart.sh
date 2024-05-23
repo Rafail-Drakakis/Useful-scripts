c22a43bd7c4d7f1a3a1784d3b2770d763300e184
Rafail-Drakakis <rafaildrakakis123@gmail.com>
2024-05-23 20:09:42 +0300

    added things to the bashscript

--- nordvpn_autostart.sh
+++ nordvpn_autostart.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# Define the service file path
-SERVICE_FILE="/etc/systemd/system/nordvpn-startup.service"
-
 # Check if the script is run as root
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root"
-   exit 1
-fi
+if [ "$(id -u)" -eq 0 ]; then
+    echo "The script is running with root privileges."
 
-# Create and write the service file
-cat << EOF > $SERVICE_FILE
+   # Define the service file path
+   SERVICE_FILE="/etc/systemd/system/nordvpn-startup.service"
+
+   # Create and write the service file
+   cat << EOF > $SERVICE_FILE
 [Unit]
 Description=Connect to NordVPN at startup
 After=network-online.target
@@ -25,14 +23,20 @@ RemainAfterExit=yes
 WantedBy=multi-user.target
 EOF
 
-# Reload systemd to recognize the new service
-echo "Reloading systemd daemon..."
-sudo systemctl daemon-reload
+   # Reload systemd to recognize the new service
+   echo "Reloading systemd daemon..."
+   sudo systemctl daemon-reload
 
-# Restart the service
-echo "Restarting nordvpn-startup.service..."
-sudo systemctl restart nordvpn-startup.service
+   # Restart the service
+   echo "Restarting nordvpn-startup.service..."
+   sudo systemctl restart nordvpn-startup.service
 
-# Check the status of the service
-echo "Checking the status of nordvpn-startup.service..."
-sudo systemctl status nordvpn-startup.service
+   # Check the status of the service
+   echo "Checking the status of nordvpn-startup.service..."
+   sudo systemctl status nordvpn-startup.service
+
+   echo "Press ctr + c"
+
+else
+    echo "Please run the script with sudo."
+fi
