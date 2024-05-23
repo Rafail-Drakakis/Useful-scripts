7d4216c3e4fb3e45a11be709d269875c20e1e1f5
Rafail-Drakakis <rafaildrakakis123@gmail.com>
2024-05-23 19:34:01 +0300

    added scripts for pip and python

--- configurator.sh
+++ configurator.sh
@@ -24,13 +24,26 @@ source ~/my_venv/bin/activate
 pip3 install torch torchvision torchaudio ffprobe PyPDF2 SpeechRecognition urllib3 matplotlib beautifulsoup4 ffmpeg tk pygame phonenumbers python-docx openpyxl numpy customtkinter ctkmessagebox qrcode pandas requests Pillow pdf2image moviepy pyshorteners pdf2docx yt-dlp tabula-py pytesseract opencv-python folium rasterio punctuators
 deactivate
 
+HOME_PATH="$HOME"
+for tool_script in "pip:run_pip.sh" "python:run_python.sh"; do
+    tool=$(echo "$tool_script" | cut -d: -f1)
+    script_name=$(echo "$tool_script" | cut -d: -f2)
+    script_path="$HOME_PATH/$script_name"
+    
+    echo "source /home/rafail/my_venv/bin/activate" >> "$script_path"
+    echo "${tool}3 \"\$@\"" >> "$script_path"
+    echo "deactivate" >> "$script_path"
+    
+    chmod +x "$script_path"
+done
+
 # Set aliases and environment variables
 echo "export MAKEFLAGS=\"-j\$(nproc)\"" >> ~/.bashrc
 echo "alias update='sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y'" >> ~/.bashrc
-echo "alias pip='source /home/rafail/my_venv/bin/activate && pip3'" >> ~/.bashrc
-echo "alias python='source /home/rafail/my_venv/bin/activate && python3'"  >> ~/.bashrc
 echo "alias connect='nordvpn connect'" >> ~/.bashrc
 echo "alias disconnect='nordvpn disconnect'" >> ~/.bashrc
+echo "alias pip='/home/rafail/run_pip.sh'" >> ~/.bashrc
+echo "alias python='/home/rafail/run_python.sh'" >> ~/.bashrc
 
 # Install and configure neovim
 git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
